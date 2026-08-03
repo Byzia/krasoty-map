91cd22ff2f28f214818ba4bfc4dd9e30797a7ee8
Byzia <byzinovskycanal@gmail.com>
2026-08-03 18:52:30 +0300

    Add files via upload

---
+++ games.js
@@ -0,0 +1,9 @@
+// Модуль управления игровым центром
+
+function initGamesTab() {
+    const container = document.getElementById('games-container');
+    if (!container) return;
+    
+    // В будущем сюда будет подключаться логика динамической загрузки меню игр или вызова отдельного файла конкретной игры.
+    console.log('Игровой центр инициализирован');
+}
